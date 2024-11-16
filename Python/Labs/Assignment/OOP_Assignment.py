967b49751d67b9ce0ae5db4667940255967097df
Ryan Pitman <inomoncatnip@hotmail.com>
2024-11-16 02:13:58 +0000

    added assignment skeleton

---
+++ Python/Labs/Assignment/OOP_Assignment.py
@@ -0,0 +1,41 @@
+# CMPU 2016 Object Oriented Programming
+# TU857-2
+# 2024/2025 Semester 1
+#
+# Group: Null Pointer
+# Members: 1. Ryan Pitman 2. Qiu Xie 3. Andrew Cotter 4. Adam Pekalski 5. Erik Hansen Lopez 6. Daniel Smyth
+#
+# A mystery adventure game where the user must solve a puzzle on each level to gather the clue for that level
+# The clues are used to solve the final puzzle and win the game
+# The game is played in the terminal and the user must input their choices
+# This project is a demonstration of object oriented programming and the use of object oriented concepts such as inheritance, encapsulation, and polymorphism
+# The project also demonstrates the use of classes, objects, and methods in Python
+
+class Game:
+    def __init__(self):
+        pass
+
+class Level:
+    def __init__(self):
+        pass
+
+class Suspect:
+    def __init__(self):
+        pass
+
+class Witness:
+    def __init__(self):
+        pass
+
+class NPC:
+    def __init__(self):
+        pass
+
+class Puzzle:
+    def __init__(self):
+        pass
+
+class Clue:
+    def __init__(self):
+        pass
+
